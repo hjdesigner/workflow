e92d28c24b9144b1ce39696677afa8f2f2f31c9c
Henrique Melanda <hjdesigner23@gmail.com>
2018-05-15 13:28:02 -0300

    Initial commit workflow webpack 4

---
+++ src/scripts/app.js
@@ -0,0 +1,4 @@
+import '../sass/main.scss';
+
+const title = document.querySelector('[data-js="title"]');
+title.innerHTML = 'Hello Webpack 4 8D';
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const styleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+	entry: { main: './src/scripts/app.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[chunkhash].js'
+  },
+  module: {
+		rules: [
+			{
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+			},
+			{
+        test: /\.scss$/,
+				use:  [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				]
+      }
+    ]
+	},
+	plugins: [
+		new CleanWebpackPlugin('dist', {} ),
+		new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+		new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+		}),
+		new WebpackMd5Hash(),
+		new styleLintPlugin({
+      configFile: '.stylelintrc',
+      context: 'src',
+      files: '**/*.css',
+      failOnError: false,
+      quiet: false,
+    })
+  ]
+};
